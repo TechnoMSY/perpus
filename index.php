c0645aaa9388254232e7ae127db99ee11fb06301
kholidsalam <kholidmsy@gmail.com>
2026-04-21 08:36:50 +0800

    perubahan index

--- index.php
+++ index.php
@@ -298,7 +298,7 @@ if (!isset($_SESSION["user"])) {
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="profil.php">
+                                <a class="dropdown-item" href="?page=profile">
                                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Profile
                                 </a>
